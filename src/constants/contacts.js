9e0360ba2311441f2ab8bd615f68c26cc4aae41c
ValkinDED <sonyerik289@gmail.com>
2025-05-03 01:49:12 +0200

    nodejs-hw-01

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,4 +1,9 @@
-const path = require('path');
+import path from 'path';
 
-const PATH_DB = path.join(__dirname, '../db/db.json');
-module.exports = { PATH_DB };
+// Получаем путь к текущему модулю
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// Генерируем правильный путь к db.json
+const PATH_DB = path.resolve(__dirname, '../db/db.json'); // Убедитесь, что путь строится верно
+
+export { PATH_DB };
